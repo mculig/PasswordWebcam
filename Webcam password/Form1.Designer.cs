e4c13edb226d7836fdbd244c5293d6a3f79cc727
Mislav Culig <mculig@gmail.com>
2019-11-07 23:05:19 +0100

    Add project files.

---
+++ Webcam password/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Webcam_password
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.camBox = new System.Windows.Forms.ComboBox();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.pathButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // camBox
+            // 
+            this.camBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.camBox.FormattingEnabled = true;
+            this.camBox.Location = new System.Drawing.Point(29, 12);
+            this.camBox.Name = "camBox";
+            this.camBox.Size = new System.Drawing.Size(512, 28);
+            this.camBox.TabIndex = 0;
+            this.camBox.SelectedIndexChanged += new System.EventHandler(this.camBox_SelectedIndexChanged);
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox.Location = new System.Drawing.Point(29, 173);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(512, 288);
+            this.picBox.TabIndex = 1;
+            this.picBox.TabStop = false;
+            // 
+            // pathBox
+            // 
+            this.pathBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pathBox.BackColor = System.Drawing.SystemColors.Window;
+            this.pathBox.Location = new System.Drawing.Point(29, 58);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.ReadOnly = true;
+            this.pathBox.Size = new System.Drawing.Size(512, 26);
+            this.pathBox.TabIndex = 2;
+            // 
+            // pathButton
+            // 
+            this.pathButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pathButton.Location = new System.Drawing.Point(216, 102);
+            this.pathButton.Name = "pathButton";
+            this.pathButton.Size = new System.Drawing.Size(150, 45);
+            this.pathButton.TabIndex = 3;
+            this.pathButton.Text = "Select Folder";
+            this.pathButton.UseVisualStyleBackColor = true;
+            this.pathButton.Click += new System.EventHandler(this.pathButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 491);
+            this.Controls.Add(this.pathButton);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.camBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WebcamPassword";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox camBox;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.Button pathButton;
+    }
+}
+
---
+++ Webcam password/Form1.cs
@@ -0,0 +1,95 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Webcam_password
+{
+    public partial class Form1 : Form
+    {
+
+        FilterInfoCollection fic;
+        VideoCaptureDevice cam;
+        EventLog myLog;
+        Bitmap lastImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnEntryWritten(object source, EntryWrittenEventArgs e)
+        {
+            
+            int e1 = myLog.Entries.Count - 1;
+            EventLogEntry last = myLog.Entries[e1];
+
+            if (last.InstanceId == 4625)
+                if (cam.IsRunning)
+                        lastImage.Save(pathBox.Text + "\\WebcamPassword_Intruder_" + DateTime.Now.ToString("yyyyMMddTHHMMss") + ".png", ImageFormat.Png);
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //AForge video stuff
+            fic = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in fic)
+                camBox.Items.Add(Device.Name);
+            cam = new VideoCaptureDevice();
+            camBox.SelectedIndex = 0;
+
+            //Eventlog stuff
+
+            myLog = new EventLog("Security");
+            myLog.EntryWritten += new EntryWrittenEventHandler(OnEntryWritten);
+            myLog.EnableRaisingEvents = true;
+
+            pathBox.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+        }
+
+        private void camBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cam.IsRunning)
+                cam.Stop();
+            cam = new VideoCaptureDevice(fic[camBox.SelectedIndex].MonikerString);
+            cam.NewFrame += newFrame;
+            cam.Start();
+        }
+
+        private void newFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            lastImage = (Bitmap)eventArgs.Frame.Clone();
+            picBox.Image = (Bitmap) lastImage.Clone();
+            picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+        private void pathButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if( dialog.ShowDialog() == DialogResult.OK)
+            {
+                pathBox.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (cam.IsRunning)
+                cam.Stop();
+        }
+    }
+}
